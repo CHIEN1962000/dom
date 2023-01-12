e45b445791c43058577d19490cdd8081c9a3f6f7
NDC\dinhc <dinhchien1962000@gmail.com>
2023-01-12 22:41:44 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+// lay ra phan tu nut bam
+let btn = document.querySelector('#btn')
+
+//Chặn sự kiện mở context menu khi chuột phải được nhấn
+btn.addEventListener('contextmenu',(e)=>{
+    e.preventDefault();
+})
+
+//Lắng nghe sự kiện và hiển thị ra nội dung nút được nhấn
+btn.addEventListener('mouseup',(e)=>{
+    let msg = document.querySelector('#message');
+    switch(e.button) {
+        case 0:
+            msg.textContent = 'Left mouse button clicked.';
+        break;
+        case 1:
+            msg.textContent = 'Middle mouse button clicked.';
+        break;
+        case 2:
+            msg.textContent = 'Right mouse button clicked.';
+        break;
+        default:
+            msg.textContent = `Unknown mouse button code: ${event.button}`;
+    }
+})
